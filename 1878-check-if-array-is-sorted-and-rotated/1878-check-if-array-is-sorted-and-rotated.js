d96e9de4de3744bfea44994bbec3ba20f222d062
Rahul Mishra <rahulmishra4317@gmail.com>
2024-11-13 13:51:55 +0530

    Time: 0 ms (100%), Space: 48.8 MB (56.14%) - LeetHub

--- 1878-check-if-array-is-sorted-and-rotated/1878-check-if-array-is-sorted-and-rotated.js
+++ 1878-check-if-array-is-sorted-and-rotated/1878-check-if-array-is-sorted-and-rotated.js
@@ -5,13 +5,11 @@
 var check = function(nums) {
     let dropCount = 0;
     let n = nums.length;
-    for(let i =0;i<n;i++) {
-        if(nums[i] > nums[(i+1) % n]) {
-            dropCount++
+    for(let i = 0;i<nums.length;i++) {
+        if(nums[i] > nums[(i+1)%n]) {
+            dropCount +=1;
         }
     }
-    if(dropCount > 1){
-        return false 
-    }
+    if(dropCount > 1) return false 
     return true 
 };
